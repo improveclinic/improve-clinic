1967cda526f1e50667d7ca318b08b52101358df9
improveclinic <amm.improveclinic@gmail.com>
2026-09-02 16:15:43 +0000

    feat(): aggiunta integrazione con Vercel Analytics e aggiornamento package-lock

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from "@vercel/analytics"
  
 injectSpeedInsights();
+inject()
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
