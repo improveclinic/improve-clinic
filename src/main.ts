1e0b6a598e2df663cb8dd24f5c6f56de9c45b46e
improveclinic <amm.improveclinic@gmail.com>
2026-09-02 16:04:44 +0000

    feat(): aggiunta integrazione con Vercel Speed Insights

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+ 
+injectSpeedInsights();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
